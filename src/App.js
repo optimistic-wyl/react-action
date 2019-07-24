7ffa1ae033da73eec3b7392628a2adc3929476ec
optimistic-wyl <461105121@qq.com>
2019-07-24 17:57:36 +0800

    更改

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {connect} from 'react-redux'
+import {Add_Gun,Remove_Gun,Add_Gun_Async } from './reducer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route } from 'react-router-dom'
+
+import Login from './container/login'
+import Register from './container/register'
+
+@connect(
+    // @符合是装饰器babel-plugin-transform-decorators-legacy实现的。
+    //第一个参数是，你要state什么属性放到props里面
+    state=>{
+        console.log('+++state+++:',state)
+        return {num:state.ct}
+    },
+    // 你要什么方法，把它放到props里，自动dispatch
+    { Add_Gun,Remove_Gun,Add_Gun_Async }
+)
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>现在有{this.props.num}把机关枪</h2>
+                <button onClick={this.props.Add_Gun}>++机关枪++</button>
+                <button onClick={this.props.Remove_Gun}>--机关枪--</button>
+                <button onClick={this.props.Add_Gun_Async}>过2秒还</button>
+                <Route path="/Register" component={Register}></Route>
+                <Route path="/login" component={Login}></Route>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/reducer.js
@@ -0,0 +1,38 @@
+const add_Gun = "Add_Gun"
+const remove_Gun = "Remove_Gun"
+
+const initialState = {
+    isAuth: false,
+    username: 'ok',
+    ct: 0
+}
+
+export default function counter(state=initialState,action){
+  switch(action.type){
+      case 'Add_Gun':
+          state.ct++
+          return Object.assign({},state)
+      case 'Remove_Gun':
+            state.ct--
+            return Object.assign({},state)
+      default :
+          return state
+  }
+}
+
+export function Add_Gun(){
+  return {type:add_Gun}
+}
+
+export function Remove_Gun(){
+  return {type:remove_Gun}
+}
+
+// 异步加载，并且可以返回一个函数了。
+export function Add_Gun_Async(){
+  return dispatch=>{
+      setTimeout(()=>{
+          dispatch(Add_Gun())
+      },2000)
+  }
+}
