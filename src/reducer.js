c854ee7c5df65bff35c93066050584e211103e9f
optimistic-wyl <461105121@qq.com>
2019-08-14 15:21:50 +0800

    thunk中间件的解释

--- src/reducer.js
+++ src/reducer.js
@@ -29,7 +29,11 @@ export function Remove_Gun(){
   return {type:remove_Gun}
 }
 
-// 异步加载，并且可以返回一个函数了。
+/**
+ * 异步加载，并且可以返回一个函数了。
+ * thunk中间件的作用：改造store.dispatch，使其可以接受函数作为参数，如下Add_Gun_Async这个action生成函数返回的是一个函数
+ * 一般store.dispatch只接受对象，如上Add_Gun\Remove_Gun这两个函数都是返回对象
+ *  */ 
 export function Add_Gun_Async(){
   Toast.loading('加载中', 0)
   return dispatch=>{
